c09f3f1a671355429c3e82e62c06458277369d0b
smilejos <smilejos.tung@gmail.com>
2017-07-18 23:36:48 +0800

    add facebook login

--- app.js
+++ app.js
@@ -5,63 +5,28 @@ let session             = require("express-session");
 let sqlStore            = require('connect-mssql')(session);
 let io                  = require('socket.io');
 let path                = require('path');
-// let passport            = require('passport');
-// let cookieParser        = require('cookie-parser')();
-// let bodyParser          = require('body-parser').urlencoded({ extended: true });
-// let FacebookStrategy    = require('passport-facebook').Strategy;
 let dbConfig            = require('./server/config/database');
 let requestRouter       = require('./server/router/requestRouter');
 let registerSocketRouter= require('./server/router/socketRouter');
 let authConfig          = require('./server/config/authentication');
 
-// Assign root path of app
-global.rootPath = __dirname;
-
+global.rootPath = __dirname; // Assign root path of app
 let app = express();
 let sessionMiddleware = session({
     store: new sqlStore(dbConfig), // options are optional
     secret: 'somesecrettoken'
-    // secret: 'keyboard cat',
-    // resave: true,
-    // saveUninitialized: true
 });
 
-// passport.use(new FacebookStrategy(authConfig.facebookAuth,
-//     function (accessToken, refreshToken, profile, cb) {
-//         console.log(accessToken, refreshToken, profile);
-//     }
-// ));
-
 app.use(ntlm());
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
-
-// app.use(cookieParser);
-// app.use(bodyParser);
 app.use(sessionMiddleware);
-// app.use(passport.initialize());
-// app.use(passport.session());
-
 app.use(requestRouter);
 app.use(express.static(path.join(__dirname, '/assets/')))
 app.use(express.static(path.join(__dirname, '/bower_components/')))
 app.use(express.static(path.join(__dirname, '/node_modules/')))
 app.use(express.static(path.join(__dirname, '/images/')))
 
-// app.get('/auth/facebook', passport.authenticate('facebook'));
-// app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), function(req, res) {
-//     // Successful authentication, redirect home.
-//     console.log('return');
-//     res.redirect('/');
-// });
-
 let server = app.listen(8888);
 let socket = io.listen(server);
-registerSocketRouter(socket, sessionMiddleware);
 
+registerSocketRouter(socket, sessionMiddleware);
 console.log("Start server with port:8888")
--- server.js
+++ server.js
@@ -1,78 +1,55 @@
-var express = require('express');
-var path = require('path');
-var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
+let express         = require('express');
+let session         = require("express-session");
+let bodyParser      = require('body-parser')
+let cookieParser    = require('cookie-parser')
+let path            = require('path');
+let passport        = require('passport');
+let Strategy        = require('passport-facebook').Strategy;
 
+let sessionMiddleware = session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+});
+let bodyParserMiddleware = bodyParser.urlencoded({
+    extended: true
+})
+let cookieParserMiddleware = cookieParser();
 
-// Configure the Facebook strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
 passport.use(new Strategy({
     clientID: '1289438404475100',
     clientSecret: 'b38ec4c5af15129830a8b53b1e0865cb',
     callbackURL: "http://localhost:8888/auth/facebook/callback",
-    passReqToCallback : true,
-    profileFields: ['id', 'emails', 'name'] //This
+    profileFields: ['id', 'email', 'name', 'gender', 'picture'] //This
 }, function (accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    console.log("enter");
-    console.log(accessToken, refreshToken, profile);
+    console.log('callback');
     return cb(null, profile);
 }));
 
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
 });
-
 
 // Create a new Express application.
 var app = express();
-
-// Configure view engine to render EJS templates.
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-//app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+app.use(cookieParserMiddleware);
+app.use(bodyParserMiddleware);
+app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.static(path.join(__dirname, '/assets/')))
+app.use(express.static(path.join(__dirname, '/bower_components/')))
+app.use(express.static(path.join(__dirname, '/node_modules/')))
+app.use(express.static(path.join(__dirname, '/images/')))
 
 // Define routes.
 app.get('/', function (req, res) {
+    console.log(req.user);
     res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/login', function (req, res) {
-    res.redirect('/');
 });
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
@@ -81,6 +58,5 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRe
         res.redirect('/');
     }
 );
-
 
 app.listen(8888);
